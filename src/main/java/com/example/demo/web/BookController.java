31820f365f0f6007d9c82c1d6f5ec5bbcef6720f
topiaswho <topias.hoyhtya@outlook.com>
2023-09-20 17:22:15 +0300

    Muutoksia pt2

--- src/main/java/com/example/demo/web/BookController.java
+++ src/main/java/com/example/demo/web/BookController.java
@@ -1,16 +1,44 @@
 package com.example.demo.web;
 
 import com.example.demo.domain.Book;
+import com.example.demo.domain.BookstoreRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class BookController {
 
+    @Autowired
+    private BookstoreRepo bookstorerepo;
+
     @GetMapping("/bookstore")
-    public String index(Model model) {
-        
+    public String bookList(Model model) {
+        List<Book> books = (List<Book>) bookstorerepo.findAll();
+        model.addAttribute("books", books);
         return "bookstore";
+    }
+    @GetMapping("/addbook")
+    public String addBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/addbook")
+    public String addBookSubmit(@ModelAttribute Book book) {
+        bookstorerepo.save(book);
+        return "redirect:/bookstore";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookstorerepo.deleteById(id);
+        return "redirect:/bookstore";
     }
 }
